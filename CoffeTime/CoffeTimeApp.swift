7601e79c201c6ad20b741b4245a64001fbfff339
alex99-dev <buliga.alexandru99@gmail.com>
2021-03-27 18:21:38 +0200

    Initial Commit

---
+++ CoffeTime/CoffeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeTimeApp.swift
+//  CoffeTime
+//
+//  Created by Buliga Alexandru on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoffeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
